68fe6f4e24e5aa5521711e48c6b350875040f54d
avallejof <tonivalle15@gmail.com>
2022-08-01 11:45:19 +0200

    Main script creation. More tests in Notebook. Data files management.

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ A streamlit geo app with data about spanish cities and regions.
 
 <br>
 
-This project has the following main dependencies: Streamlit, Pandas, Geopandas and Plotly.
+This project has the following main dependencies: Streamlit, Pandas, and Plotly.
 
 Install the required pachages using:
 
     python -m pip install -r requirements.txt
-
-If fiona (A dependency for geopandas) isn't installing, an error common on windows systems, you can find the wheels for the package [here](https://www.lfd.uci.edu/~gohlke/pythonlibs/#fiona) or install via conda.
 
 <br>
 
@@ -26,9 +24,7 @@ To Start this project in your localhost type on a console while on the project f
 
 Geo data has been sourced from:
 
-- https://simplemaps.com/data/es-cities
-
-- https://geographica.carto.com/u/alasarr/tables/spain_provinces/public?redirected=true
+- https://public.opendatasoft.com/explore/dataset/provincias-espanolas/table/?sort=provincia
 
 Rent and house price data sourced from:
 
@@ -44,3 +40,7 @@ Other sources:
 - https://github.com/codeforgermany/click_that_hood/blob/main/public/data/spain-provinces.geojson
 
 - https://www.ine.es/dyngs/INEbase/en/operacion.htm?c=Estadistica_C&cid=1254736176951&menu=ultiDatos&idp=1254735572981
+
+- https://simplemaps.com/data/es-cities
+
+- https://geographica.carto.com/u/alasarr/tables/spain_provinces/public?redirected=true
---
+++ main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import json
+
+# Page configuration and title
+st.set_page_config(page_title='Map Testing', layout='wide', page_icon="🌍")
+# st.markdown("<h1 style='text-align: center; color: #666666;'>Map Testing.</h1>", unsafe_allow_html=True)
+col1, col2 = st.columns([1,10])
+
+# Type of graph
+plot = col1.radio(
+     "Graph:",
+     ("Population", "Rent House", "Buy House"))
+
+# geojson load
+# Preparamos el GeoJSON
+provincias = json.load(open("./data/raw sources/provincias-espanolas.geojson", "r"))
+provincias_id = {}
+for feature in provincias["features"]:
+    feature["id"] = feature["properties"]["codigo"]
+    provincias_id[feature["properties"]["provincia"]] = feature["id"]
+provincias_id['Baleares'] = provincias_id.pop('Illes Balears')
+provincias_id['Vizcaya'] = provincias_id.pop('Bizkaia')
+provincias_id['Alicante/Alacant'] = provincias_id.pop('Alacant')
+provincias_id['Castellón/Castelló'] = provincias_id.pop('Castelló')
+provincias_id['Álava'] = provincias_id.pop('Araba')
+provincias_id['Guipúzcoa'] = provincias_id.pop('Gipuzcoa')
+
+if plot == "Population":
+    # Dataset load
+    df = pd.read_pickle("data/pop_df.pkl")
+    fig = px.density_mapbox(df, lat="lat", lon="lng", z="population"
+                        ,hover_name='city'
+                        ,center=dict(lat=39, lon=-3.7)
+                        # ,range_color = [0, 200]
+                        ,zoom=4
+                        ,radius=50
+                        ,opacity=0.5
+                        ,mapbox_style='open-street-map'
+                        ,height=800
+                        ,title="<b>Spanish population by city</b>"
+                        )
+    fig.update_layout(title={'font': {'size': 50}})
+
+elif plot == "Rent House":
+    df = pd.read_pickle("data/rent_df.pkl")
+    fig = px.choropleth(df,locations="id", color="Rent 100m2 house",
+                        geojson=provincias,
+                        hover_name="Localización",
+                        height=800
+                        ,title="<b>Rent by province in Spain</b>"
+                        )             
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(title={'font': {'size': 50}})
+
+else:
+    df = pd.read_pickle("data/buy_df.pkl")
+    fig = px.choropleth(df,locations="id", color="Buy 100m2 house",
+                        geojson=provincias,
+                        hover_name="Localización",
+                        height=800
+                        ,title="<b>House price by province in Spain</b>"
+                        )
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(title={'font': {'size': 50}})
+
+col2.plotly_chart(fig, use_container_width=True)
